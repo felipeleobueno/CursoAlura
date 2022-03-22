61ce228caac08d9ad0d166a59d6e349d43d8f7a7
Felipe Leo <morrocoy.leo@hotmail.com>
2022-03-22 18:57:10 -0300

    first commit

---
+++ type-number.js
@@ -0,0 +1,24 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero / segundoNumero ;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante
+
+const numeroFlutuante = 3.3;
+const pontoFlutuante = .5;
+
+const novaOperacao = primeiroNumero / numeroFlutuante;
+
+console.log(novaOperacao)
+
+ // NaN -> Not a Number (não é um número)
+
+ const alura = "Alura";
+ console.log(alura * primeiroNumero)
---
+++ type-string.js
@@ -0,0 +1,18 @@
+const texto1 = "Olá, Mundo!";
+const texto2 = 'Olá Mundo!0';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu Nomé é ";
+const meuNome = "Felipe";
+
+
+// concatenacao (+)
+
+
+console.log(citacao + meuNome);
+
+
+
+// template string Ou template literal
+
